75adb6c1d000c0d74009c9d3f99126fd5e673d99
kunmingqaq@163.com <kunmingqaq@163.com>
2020-12-21 19:32:31 +0800

    openfeign服务调用

--- gulimall-member/src/main/java/com/lis2/gulimall/member/controller/MemberController.java
+++ gulimall-member/src/main/java/com/lis2/gulimall/member/controller/MemberController.java
@@ -3,6 +3,7 @@ package com.lis2.gulimall.member.controller;
 import java.util.Arrays;
 import java.util.Map;
 
+import com.lis2.gulimall.member.feign.CouponFeignService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -30,13 +31,24 @@ public class MemberController {
     @Autowired
     private MemberService memberService;
 
+    @Autowired
+    private CouponFeignService couponFeignService ;
+
+    @RequestMapping("/coupon")
+    public R test() {
+        MemberEntity memberEntity = new MemberEntity ();
+        memberEntity.setNickname ("李四");
+        R r = couponFeignService.memberCoupons ();
+        return R.ok ().put ("memeber", memberEntity).put ("coupon",r.get("coupon"));
+
+    }
+
     /**
      * 列表
      */
     @RequestMapping("/list")
     public R list(@RequestParam Map<String, Object> params){
         PageUtils page = memberService.queryPage(params);
-
         return R.ok().put("page", page);
     }
 
